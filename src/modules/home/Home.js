0edddc76d635e79d117b50a212c9041e39c7c577
Dani <danieltani@servicioexterno.inditex.com>
2020-04-29 09:24:56 +0200

    Adaptación de la home a clase de react

--- src/modules/home/Home.js
+++ src/modules/home/Home.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import './home.scss';
 
-function Home(props) {
+/*function Home(props) {
   return (
     <div className="home">
       <div className="home__list">
-
+        Hola mundo
+        <div className="home item">Item</div>
       </div>
     </div>
   );
+} */
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <div className="home__list">
+          Hola mundo
+          <div className="home item">Item</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
