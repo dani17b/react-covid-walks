f3a2a84d54abc93b8e68e5a454150d848a1d697b
Dani <danieltani@servicioexterno.inditex.com>
2020-04-29 13:25:08 +0200

    Adaptacion del Login para utilizar el hook de estado

--- src/modules/login/Login.js
+++ src/modules/login/Login.js
@@ -1,62 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import './login.scss';
 import { login } from './actions';
 
-class Login extends React.Component {
-  // function Login(props) {
-  //const { login, loading } = props;
+const Login = props => {
+  const { login, loading } = props;
 
-  /* const [loginInfo, setLoginInfo] = useState({
-    login: '',
-    password: ''
-  }); */
-  constructor(props) {
-    super(props);
+  const [userLogin, setUserLogin] = useState({ login: '', password: '' });
 
-    this.state = {
-      login: '',
-      password: ''
-    };
-  }
+  return (
+    <div className="login">
+      {!loading && (
+        <form
+          className="login__form"
+          onSubmit={e => {
+            e.preventDefault();
 
-  render() {
-    const { login, loading } = this.props;
-
-    return (
-      <div className="login">
-        {!loading && (
-          <form
-            className="login__form"
-            onSubmit={e => {
-              e.preventDefault();
-
-              login({...this.state});
-              // login(loginInfo);
-            }}
-          >
-            <input
-              type="text"
-              className="login__form__input"
-              placeholder="Nombre de usuario"
-              value={this.state.login}
-              onChange={e => this.setState({ login: e.target.value })}
-            />
-            <input
-              type="password"
-              className="login__form__input"
-              placeholder="Contraseña"
-              value={this.state.password}
-              onChange={e => this.setState({ password: e.target.value })}
-            />
-            <button className="login__form__button">Iniciar sesión</button>
-          </form>
-        )}
-        {loading && <div className="login__loading">Cargando</div>}
-      </div>
-    );
-  }
-}
+            login(userLogin);
+          }}
+        >
+          <input
+            type="text"
+            className="login__form__input"
+            placeholder="Nombre de usuario"
+            value={userLogin.login}
+            onChange={e => setUserLogin({ ...userLogin, login: e.target.value })}
+          />
+          <input
+            type="password"
+            className="login__form__input"
+            placeholder="Contraseña"
+            value={userLogin.password}
+            onChange={e => setUserLogin({ ...userLogin, password: e.target.value })}
+          />
+          <button className="login__form__button">Iniciar sesión</button>
+        </form>
+      )}
+      {loading && <div className="login__loading">Cargando</div>}
+    </div>
+  );
+};
 
 export default connect(
   store => ({
