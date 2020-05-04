d401e029634a5b61d2a9b1eaf2d8297a9d6144f3
Dani <danieltani@servicioexterno.inditex.com>
2020-05-04 10:37:24 +0200

    Desarrollo del primer test de login con cypress para los dos tipos de usuarios

--- cypress/integration/01_Login.spec.js
+++ cypress/integration/01_Login.spec.js
@@ -1,12 +1,35 @@
-describe ('01 Login', () => {
-    it ('Login', () => {
-      cy.visit('/');
-      cy.get('[type="text"]').type("test")
-      cy.get('[type="password"]').type("1234")
-      cy.get('.login__form__button').click()
-    });
+import Config from "./Config";
 
-    it ('Check user can see home', () => {
-        cy.get('.home').should('be.visible') 
+describe ('01 Login', () => {
+
+    Config.users.forEach(user => {
+      it('Login user (' + user.login + ')', () => {
+        // Ir a inicio
+        cy.visit('/');
+  
+        // Introducir login
+        cy.get('[type="text"].login__form__input').type(user.login)
+  
+        // Introducir password
+        cy.get('[type="password"].login__form__input').type(user.password)
+  
+        // Click en el boton de login
+        cy.get('.login__form__button').click()
+      })
+  
+      it('Check user can see homepage (' + user.login + ')', () => {
+        // Comprobar que el usuario esta en la pagina de inicio
+        cy.get('.home').should('be.visible')
+        cy.get('.users > div').should('have.length', user.numFriends);
+      })
     })
+
+    //it('Login user not registered', () => {
+      // TODO hacer el codigo de este test
+      // Ir a inicio
+      // Introducir login no registrado
+      // Introducir password
+      // Click en el boton de login
+      // Comprobar que sale un mensaje de error al hacer el login
+    //})
 });
---
+++ cypress/integration/Config.js
@@ -0,0 +1,16 @@
+const Config = {
+  users: [
+    {
+      login: 'pepe',
+      password: '1234',
+      numFriends: 4
+    },
+    {
+      login: 'juan',
+      password: '1234',
+      numFriends: 2
+    }
+  ]
+};
+
+export default Config;
